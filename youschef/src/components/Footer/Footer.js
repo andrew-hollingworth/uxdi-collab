7f7f5656b9f019e22e8767a0be7c9cd96f2315a6
darrinim <darrinj.im@gmail.com>
2019-09-23 17:50:06 -0400

    completed header mobile style

--- youschef/src/components/Footer/Footer.js
+++ youschef/src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 
 export default function Footer() {
   return (
-    <div>
+    <div className="footer">
       <h4><i className="fab fa-instagram"></i> <i className="fab fa-facebook-square"></i> <i className="fab fa-twitter-square"></i> </h4>
     </div>
   )
