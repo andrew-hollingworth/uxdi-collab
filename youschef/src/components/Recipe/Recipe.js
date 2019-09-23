a3aa5b9e6de42c92d798d2b510b99e25f53d9291
darrinim <darrinj.im@gmail.com>
2019-09-23 19:05:15 -0400

    finished single recipe container and customize button

--- youschef/src/components/Recipe/Recipe.js
+++ youschef/src/components/Recipe/Recipe.js
@@ -3,8 +3,15 @@ import './Recipe.css'
 
 export default function Recipe() {
   return (
-    <div>
-      <h1>This is a recipe.</h1>
+    <div className="recipeContainer">
+      <div className="singleRecipeContainer">
+        <div className="recipeNameContainer">
+          <h3 className="recipeName">Caesar Salad</h3>
+          <i class="fas fa-stopwatch stopwatch"></i>
+        </div>
+        <button className="customizeButton">Customize ></button>
+      </div>
+      <button className="scheduleButton">Schedule Delivery</button>
     </div>
   )
 }
